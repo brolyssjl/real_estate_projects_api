f3b2cf9e4126d3a3a00ce9e620cfa3ef50ff38d6
Jonatan Bonilla <pescadin138@gmail.com>
2022-02-14 22:35:14 -0300

    add seed for project entity using factory-bot and faker

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
   validates :name, :country, :city, :neighborhood, :address,
-            :phone, :description, :stratum, :enabled,
+            :phone, :description, :stratum,
             :sales_room_address, :sales_room_phone, :sales_room_email,
             :estimated_start_date, :estimated_end_date, presence: true
   validates :logo, presence: false, allow_blank: true
--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,12 +1,20 @@
+require 'faker'
+
 FactoryBot.define do
   factory :project do
-    name { "MyString" }
-    description { "MyText" }
-    address { "MyString" }
-    city { "MyString" }
-    country { "MyString" }
-    logo { "MyString" }
-    start_date { "2022-02-13 21:54:01" }
-    end_date { "2022-02-13 21:54:01" }
+    name { Faker::Company.name }
+    description { Faker::Lorem.paragraph(sentence_count: 6, supplemental: true) }
+    address { Faker::Address.street_address }
+    neighborhood { Faker::Address.street_name }
+    city { Faker::Address.city }
+    country { Faker::Address.country }
+    stratum { Faker::Number.between(from: 1, to: 7) }
+    logo { Faker::LoremPixel.image }
+    phone { Faker::PhoneNumber.cell_phone }
+    sales_room_address { Faker::Address.street_address }
+    sales_room_phone { Faker::PhoneNumber.phone_number }
+    sales_room_email { Faker::Internet.email }
+    estimated_start_date { Faker::Time.between(from: DateTime.now - 1, to: DateTime.now, format: :short) }
+    estimated_end_date { Faker::Time.between(from: DateTime.now - 1, to: DateTime.now + 360, format: :short) }
   end
 end
