95dd0636cc95120fe07a2e1bbfde818e05ffd7d0
Jonatan Bonilla <pescadin138@gmail.com>
2022-02-15 16:47:38 -0300

    add shoulda-matcher gem, add project model specs, fix request specs for health_check and project actions like index and show

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:country) }
+  it { should validate_presence_of(:city) }
+  it { should validate_presence_of(:neighborhood) }
+  it { should validate_presence_of(:address) }
+  it { should validate_presence_of(:phone) }
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:stratum) }
+  it { should validate_presence_of(:sales_room_address) }
+  it { should validate_presence_of(:sales_room_phone) }
+  it { should validate_presence_of(:sales_room_email) }
+  it { should validate_presence_of(:estimated_start_date) }
+  it { should validate_presence_of(:estimated_end_date) }
+  it { should allow_value("").for(:logo) } 
+  it { should validate_numericality_of(:stratum).only_integer }
 end
--- spec/requests/projects_spec.rb
+++ spec/requests/projects_spec.rb
@@ -1,18 +1,49 @@
 require 'rails_helper'
 
-RSpec.describe "Projects", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/projects/index"
-      expect(response).to have_http_status(:success)
+RSpec.describe 'Projects', type: :request do
+  # initialize test data
+  let(:project_id) { 1 }
+
+  before(:all) do
+    Rails.application.load_seed
+  end
+
+  describe 'GET /projects' do
+    before { get '/projects' }
+
+    context 'when exist projects' do
+      it 'returns a non empty response' do
+        expect(response.body).not_to be_empty
+      end
+
+      it 'returns http success' do
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 
-  describe "GET /show" do
-    it "returns http success" do
-      get "/projects/show"
-      expect(response).to have_http_status(:success)
+  describe 'GET /projects/:project_id' do
+    before { get "/projects/#{project_id}" }
+
+    context 'project found' do
+      it 'return project data' do
+        expect(response.body).not_to be_empty
+        project = JSON.parse(response.body)
+        expect(project['id']).to equal(project_id)
+      end
+
+      it 'returns http success' do
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'project not found' do
+      let(:project_id) { 100 }
+
+      it 'returns http not found' do
+        get '/projects/100'
+        expect(response).to have_http_status(:not_found)
+      end
     end
   end
-
 end
